c18100888a9e8033a4c5f72bd85ff833cfc70b88
MaxHiit <pro.maxcode@gmail.com>
2022-04-21 23:24:44 +0200

    feature # add navbar + setup route link

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+	<header class="layout">
+		<h1 class="title">Home</h1>
+		<nav class="navbar">
+			<ul class="navbar-list">
+				<router-link :to="{ name: 'home' }" class="navbar-item">Home</router-link>
+				<router-link :to="{ name: 'blog' }" class="navbar-item">Blog</router-link>
+				<router-link :to="{ name: 'create' }" class="navbar-item">Add post</router-link>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Navbar'
+};
+</script>
+
+<style>
+header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+
+.navbar {
+	padding: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<main class="layout home">
+		<h1>Home</h1>
+	</main>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+	name: 'Home',
+	components: {}
+};
+</script>
+
+<style>
+.home {
+	height: calc(100vh - 1rem);
+}
+</style>
