a38ff80c170535664edf1cb9bed7f21b6c04ae50
MaxHiit <pro.maxcode@gmail.com>
2022-04-28 02:29:26 +0200

    refacto code

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,9 @@
 <template>
 	<header class="layout">
-		<h1 class="title">Home</h1>
-		<nav class="navbar">
-			<ul class="navbar-list">
+		<nav class="navbar w-full">
+			<ul class="navbar-list flex items-center justify-center gap-x-8">
 				<router-link :to="{ name: 'home' }" class="navbar-item">Home</router-link>
 				<router-link :to="{ name: 'blog' }" class="navbar-item">Blog</router-link>
-				<router-link :to="{ name: 'create' }" class="navbar-item">Add post</router-link>
 			</ul>
 		</nav>
 	</header>
@@ -17,14 +15,4 @@ export default {
 };
 </script>
 
-<style>
-header {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-}
-
-.navbar {
-	padding: 0;
-}
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,47 @@
 <template>
 	<main class="layout home">
-		<h1>Home</h1>
+		<h1 class="page-title text-4xl">Home</h1>
+		<p class="mb-10">
+			Leace a comment below. It could be anything!<br />
+			Appreciation, Information, Wisdom, or even Humor. Surprise me!
+		</p>
+
+		<router-link :to="{ name: 'create' }" class="btn">Add post</router-link>
+		<div
+			class="flex justify-between items-center py-2.5 my-10 border-y-[1px] border-[#ececec] border-solid"
+		>
+			<h2>Latest Post</h2>
+			<router-link :to="{ name: 'blog' }" class="text-xs underline underline-offset-1"
+				>View all</router-link
+			>
+		</div>
+
+		<Post />
 	</main>
 </template>
 
 <script>
+import Post from '@/components/SinglePost.vue';
 // @ is an alias to /src
 
 export default {
 	name: 'Home',
-	components: {}
+	components: {
+		Post
+	}
 };
 </script>
 
 <style>
 .home {
 	height: calc(100vh - 1rem);
+}
+
+.page-title {
+	margin-bottom: 1.5rem;
+}
+
+.page-subtitle {
+	margin-bottom: 1.2rem;
 }
 </style>
